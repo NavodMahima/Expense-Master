abdd98c6cd0c6ad9c6f20bd4c04a3277a5b6c5f2
Navod Mahima <gnmshihara@std.appsc.sab.ac.lk>
2024-04-16 21:26:08 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Expences(),
     );
   }
 }
---
+++ lib/pages/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class expense extends StatefulWidget {
+  const expense({super.key});
+
+  @override
+  State<expense> createState() => _expenseState();
+}
+
+class _expenseState extends State<expense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
